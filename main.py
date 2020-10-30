d46ddc97ce87cf831719fbfabb7f450475b73ca0
julien denos <juju68@gmail.com>
2020-10-30 17:40:23 +0100

    Add requirements type environement change code from local file to api file from NYC open data

--- main.py
+++ main.py
@@ -22,11 +22,11 @@ The purpose of this dashboard is to visualise vehicle collision 🚗 in New York
 
 @st.cache(persist=True)
 def get_data(nrows=None):
-    data = pd.read_csv(data_file, nrows=nrows, parse_dates=[['CRASH_DATE', 'CRASH_TIME']])
+    data = pd.read_csv('https://data.cityofnewyork.us/resource/hhsa-x92p.csv', nrows=nrows, parse_dates=[['CRASH DATE', 'CRASH TIME']])
     data.dropna(subset=['LATITUDE', 'LONGITUDE'], inplace=True)
     data.rename(lambda x: str(x).lower(), axis='columns', inplace=True)
-    data.rename(columns={'crash_date_crash_time': 'date_time'}, inplace=True)
-    data['time'] = data['date_time'].dt.time
+    data.rename(columns={'crash date_crash time': 'date_time'}, inplace=True)
+    #data['time'] = data['date_time'].dt.time
     data.reset_index(inplace=True, drop=True)
     return data
 
@@ -34,8 +34,8 @@ def get_data(nrows=None):
 # nrows= 100000 # st.slider('How many row do you want to fetch ?',10000,200000000,step=100000)
 df = get_data(1000)
 
-if st.checkbox('View Data', False):
-    st.subheader('View Data')
+if st.checkbox('Raw Data', False):
+    st.subheader('Raw Data')
     # st.write(df)  # TODO use plotly table see if it solve the unexistant date problem
     fig = go.Figure(data=[go.Table(
         header=dict(values=list(df.columns),
@@ -53,7 +53,7 @@ if st.checkbox('View Data', False):
 # Filters
 st.sidebar.header("Filters")
 injured_people = st.sidebar.slider("Number of people injured in the accident (or more)", 0,
-                                   int(df['injured_persons'].max()),
+                                   int(df['number of persons injured'].max()),
                                    value=2)
 time = st.sidebar.slider("What time period do you want to check", min_value=dt.time(00, 00), max_value=dt.time(23, 59),
                          value=[dt.time(12, 00), dt.time(20, 00)], step=dt.timedelta(minutes=15))
@@ -67,7 +67,7 @@ df_filtred = df[
     & (df['date_time'].dt.time <= time[1])
     & (df['date_time'].dt.date >= date[0])
     & (df['date_time'].dt.date <= date[1])]\
-    .query('injured_persons >= @injured_people')
+    .query('`number of persons injured` >= @injured_people')
 
 # 2D map
 st.header('Location of accidents:')
